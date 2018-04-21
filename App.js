d8348327ff6321b10d64ef78cb3bafa884106897
Emily Zhong <emilyzhongsihua@gmail.com>
2018-04-21 15:08:00 -0700

    components

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
